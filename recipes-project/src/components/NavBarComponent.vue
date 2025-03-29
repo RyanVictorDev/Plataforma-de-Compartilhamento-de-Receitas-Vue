5e89a2b3e204706a5556f94263613e189582099c
RyanVictorDev <vryan8294@gmail.com>
2025-03-29 10:00:51 -0300

    Init project

---
+++ recipes-project/src/components/NavBarComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app-bar :elevation="2" theme="white" color="white">
+    <v-app-bar-title class="font-weight-bold">RecipeShare</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+</script>
