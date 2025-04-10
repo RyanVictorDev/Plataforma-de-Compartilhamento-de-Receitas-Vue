8c0d8299846e09e80b181cf5c6d5672329e6488e
RyanVictorDev <vryan8294@gmail.com>
2025-04-10 10:50:13 -0300

    teste

--- recipes-project/src/components/Home.vue
+++ recipes-project/src/components/Home.vue
@@ -4,7 +4,9 @@
       <div class="banner mb-5">
         <div class="bannerContent d-flex flex-column text-white ml-5 ga-2 my-auto">
           <p class="text-h4 font-weight-bold">Compartilhe suas criações culinárias</p>
-          <p class="text-body-1">Descubra receitas incríveis de todo o mundo ou compartilhe as suas para todos os amantes de comida</p>
+          <p class="text-body-1">
+            Descubra receitas incríveis de todo o mundo ou compartilhe as suas para todos os amantes de comida
+          </p>
 
           <div class="d-flex ga-8">
             <v-btn size="large" class="text-orange" color="white" to="/recipes">Explorar Receitas</v-btn>
@@ -12,15 +14,28 @@
           </div>
         </div>
       </div>
-      <v-row class="d-flex">
-        <v-col v-for="recipe in recipes" :key="recipe.id" cols="12" sm="6" md="4" lg="3">
-          <recipe-component
-            :id="String(recipe.id)"
-            :user-id="String(recipe.user.id)"
-            :title="recipe.title"
-            :description="recipe.description"
-            :tag="recipe.tag"
-          />
+
+      <v-row>
+        <v-col cols="12">
+          <p class="text-h5 font-weight-bold mb-2">Últimas receitas compartilhadas</p>
+
+          <div class="scroll-wrapper">
+            <div class="scroll-content">
+              <div
+                v-for="recipe in recipes"
+                :key="recipe.id"
+                class="scroll-item"
+              >
+                <recipe-component
+                  :id="String(recipe.id)"
+                  :user-id="String(recipe.user.id)"
+                  :title="recipe.title"
+                  :description="recipe.description"
+                  :tag="recipe.tag"
+                />
+              </div>
+            </div>
+          </div>
         </v-col>
       </v-row>
     </v-responsive>
@@ -73,9 +88,42 @@ const getRecipes = () => {
   justify-content: left;
 }
 
-
 .bannerContent{
   max-width: 55%;
   max-height: 30%;
+}
+
+.scroll-wrapper {
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch;
+  padding-bottom: 8px;
+}
+
+.scroll-wrapper::-webkit-scrollbar {
+  height: 8px;
+}
+
+.scroll-wrapper::-webkit-scrollbar-track {
+  background: #f0f0f0;
+  border-radius: 4px;
+}
+
+.scroll-wrapper::-webkit-scrollbar-thumb {
+  background-color: #ff9900b0;
+  border-radius: 4px;
+  transition: background-color 0.5s ease-in-out;
+}
+
+.scroll-wrapper::-webkit-scrollbar-thumb:hover {
+  background-color: #ff8c00;
+}
+
+.scroll-content {
+  display: flex;
+}
+
+.scroll-item {
+  min-width: 280px;
+  flex-shrink: 0;
 }
 </style>
